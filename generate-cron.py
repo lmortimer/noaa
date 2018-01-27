19c591fc368fc9aa783ffbfb609ebdfda70573b8
Logan Mortimer <logan@isthisit.nz>
2018-01-27 13:53:32 +1300

    Cron doesn't like command substitution. Move each call into a shell script

--- generate-cron.py
+++ generate-cron.py
@@ -5,8 +5,10 @@ import os
 
 
 ABSOLUTE_PATH = os.path.dirname(os.path.realpath(__file__))
-JOB_STR = '{cron} {absolute_path}/get.py {url} > {absolute_path}/{directory}$(date -d "today" +"%Y-%m-%d-%H%M%S").txt'
+JOB_STR = '{absolute_path}/get.py {url} > {absolute_path}/{directory}$(date -d "today" +"%Y-%m-%d-%H%M%S").txt'
+CRON_STR = '{cron} {absolute_path}/scripts/{id}.sh'
 
+os.makedirs('scripts', exist_ok=True)
 configuration = json.loads(open("sources.json", "r").read())
 
 for item in configuration:
@@ -15,7 +17,14 @@ for item in configuration:
 
     os.makedirs(output_directory, exist_ok=True)
 
-    print(JOB_STR.format(absolute_path=ABSOLUTE_PATH,
-                         cron=item['cron'],
-                         url=item['url'],
-                         directory=output_directory))
+    with open('scripts/{}.sh'.format(item['id']), 'w') as fh:
+        fh.write('#!/bin/bash\n')
+        fh.write(JOB_STR.format(absolute_path=ABSOLUTE_PATH,
+                                url=item['url'],
+                                directory=output_directory))
+
+    os.chmod('scripts/{}.sh'.format(item['id']), 0o755)
+
+    print(CRON_STR.format(cron=item['cron'],
+                          absolute_path=ABSOLUTE_PATH,
+                          id=item['id']))
